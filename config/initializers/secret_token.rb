34c2ff4217d1d8e0bf200092f9f23bf7929e5bb9
Dave Dosi <daviddosi@gmail.com>
2010-09-15 18:56:03 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pipedream::Application.config.secret_token = '38b90c20fd96f8f3ac33e587749a944260cb4a5ba73368d9b01e1fa8b56dc26d244570feae7c1316b6d89eb94860f80e6c0eea95f3c97e2a00c6700517ffef5b'
